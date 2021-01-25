592e6269e4b37ba18a333831c8976042560e22b9
billygoat97 <kim_ki_hyun_@naver.com>
2021-01-26 00:52:50 +0900

    Add files via upload

---
+++ 1676_factorial.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+    int sum = 0;
+    while(n!=0){
+
+        sum += n/5;
+        n /=5;
+    }
+    
+    cout<<sum<<endl;
+
+    return 0;
+}
