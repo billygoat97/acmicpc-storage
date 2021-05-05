85b12f57600bcdf93b81b17a231187f98cc93e2d
Kim Ki Hyun <kim_ki_hyun_@naver.com>
2021-05-05 16:03:10 +0900

    Update 1966_queue.cpp

--- 1966_queue.cpp
+++ 1966_queue.cpp
@@ -1,35 +1,50 @@
 #include <iostream>
-#include <cmath>
+#include <vector>
 #include <queue>
+#include <algorithm>
 #include <cstring>
+#include <stack>
 
 using namespace std;
 
 int main(){
+    
+    
+    int n;
+    cin >>n;
+    for(int i = 0; i<n; i++){
+        vector<int> v;
+        queue<pair<int,int> > q;
+        int num, when,cnt = 0;
+        cin >> num >> when;
+        for(int j = 0; j<num;j++){
 
-    int num;
-    cin >>num;
-    queue<int> q;
-    queue<int> q_sort;
-    for(int i = 0; i<num;i++){
-
-        int t_cnt, when;
-        cin >>t_cnt>>when;
-        int max = 0;
-        for(int j = 0; j<t_cnt; j++){
-            int n;
-            cin >> n;
-            q.push(n);
-            q_sort.push(n);
+            int jungyo;
+            cin>> jungyo;
+            q.push(make_pair(j,jungyo));
+            v.push_back(jungyo);
         }
-
-
-
-
-
+        sort(v.rbegin(),v.rend());
+        int ptr=0;
+        while(q.empty() == false){
+            // cout<<cnt;
+            // cout<<endl;
+            if(q.front().second == v[ptr]){
+                cnt++;
+                ptr++;
+                if(q.front().first == when)break;
+                q.pop();
+            }else{
+                q.push(q.front());
+                q.pop();
+            }
+        }
+        while(q.empty() == false){
+            q.pop();
+        }
+        cout<<cnt<<endl;
+        // v.clear();
     }
-
-
 
     return 0;
 }
