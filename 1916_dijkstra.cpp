bd8d610753a70e46edcfd84b922ae4c6afa9a314
billygoat97 <kim_ki_hyun_@naver.com>
2021-07-29 12:38:23 +0900

    20210728_dijkstra 문제

---
+++ 1916_dijkstra.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <queue>
+#include <algorithm>
+#include <cmath>
+#define INF 987654321
+using namespace std;
+
+int n,m;
+vector<pair<int, int> > arr[1001];
+int d[1001];
+
+
+void dijkstra(int start){
+  priority_queue<pair<int,int>>pq;
+  pq.push({0,start});
+  d[start] = 0;
+  while(pq.size()!= 0){
+    int dist = -pq.top().first;
+    int now = pq.top().second;
+    pq.pop();
+    if(d[now]<dist)continue;
+    for(int i = 0; i<arr[now].size();i++){
+      int cost = dist + arr[now][i].second;
+      if(cost < d[arr[now][i].first]){
+        d[arr[now][i].first] = cost;
+        pq.push(make_pair(-cost,arr[now][i].first));
+      }
+    }
+    
+  }
+}
+
+int main(){
+  ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+  cin >> n >> m;
+  fill(d,d+n, INF);
+  for(int i = 0; i<m; i++){
+    int a,b,c;
+    cin >> a >> b >> c;
+    arr[a-1].push_back({b-1,c});
+  }
+  int start,dest;
+  cin >> start >> dest;
+  dijkstra(start-1);
+  cout<<d[dest-1]<<endl;
+  return 0;
+  
+}
